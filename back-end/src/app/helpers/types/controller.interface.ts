3991ba4c59dcb1da4a420c9e5ddc55330a8eced7
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-05-18 16:33:54 -0300

    feat: add data transfer object for controller

---
+++ back-end/src/app/helpers/types/controller.interface.ts
@@ -0,0 +1,11 @@
+import {
+  ICreateInput, IDeleteInput, IUpdateInput, IUpdateStatusInput,
+} from './task.interface';
+
+export interface ControllerDTO<T> {
+  create(obj: ICreateInput): Promise<T>;
+  read(): Promise<Array<T>>;
+  updateStatus(obj: IUpdateStatusInput): Promise<T | null>;
+  update(obj: IUpdateInput): Promise<T | null>;
+  delete(obj: IDeleteInput): Promise<T | null>;
+}
