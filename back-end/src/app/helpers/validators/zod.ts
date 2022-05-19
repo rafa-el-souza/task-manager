9127942be2f56d1f87a83565cb449f7716e95654
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-05-19 18:03:55 -0300

    feat: add zod schemas and zod task inferred type

---
+++ back-end/src/app/helpers/validators/zod.ts
@@ -0,0 +1,29 @@
+import { z } from 'zod';
+
+export const zodCreateTaskSchema = z.object({
+  name: z.string({
+    required_error: 'name is required',
+    invalid_type_error: 'name must be a string',
+  }).min(3, { message: 'name must be 3 or more characters long' }),
+
+  description: z.optional(z.string({
+    invalid_type_error: 'description must be a string',
+  })),
+
+  status: z.optional(z.enum(['pendente', 'andamento', 'pronto'])),
+});
+
+export const zodUpdateTaskSchema = z.object({
+  name: z.optional(z.string({
+    required_error: 'name is required',
+    invalid_type_error: 'name must be a string',
+  }).min(3, { message: 'name must be 3 or more characters long' })),
+
+  description: z.optional(z.string({
+    invalid_type_error: 'description must be a string',
+  })),
+
+  status: z.optional(z.enum(['pendente', 'andamento', 'pronto'])),
+});
+
+export type ZodTask = z.infer<typeof zodCreateTaskSchema>;
