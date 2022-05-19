a1aa337a036df322a2b751b7303cb93204d3f0ea
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-05-19 17:54:47 -0300

    feat: add mongoose connection to TaskManager db

---
+++ back-end/src/db/connection.ts
@@ -0,0 +1,7 @@
+import mongoose from 'mongoose';
+
+const MONGO_DB_URL = 'mongodb://localhost:27017/TaskManager';
+
+export const connection = (
+  mongoDatabaseURI = process.env.MONGO_URI || MONGO_DB_URL,
+) => mongoose.connect(mongoDatabaseURI);
