2efee809cf9c00c26d91499a32d908e232728f30
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-05-24 17:34:05 -0300

    feat: use redux state to manage tasks array | add redux state to manage whether a form to add tasks is rendered | add redux state to trigger the effect when db changes

--- front-end/src/components/TaskList.jsx
+++ front-end/src/components/TaskList.jsx
@@ -1,18 +1,36 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 import api from '../helpers/axios';
 import Task from './Task';
+import { replaceAll } from '../redux/reducers/taskReducer';
+import TaskForm from './TaskForm';
 
 function TaskList() {
-  const [tasks, setTasks] = useState([]);
+  const dispatch = useDispatch();
+
+  const tasks = useSelector((state) => state.tasks.tasks);
+
+  const addTask = useSelector((state) => state.tasks.addTask);
+
+  const reloadList = useSelector((state) => state.tasks.reloadList);
+
   useEffect(() => {
     const fetchData = async () => {
-      const tasksFromApi = await api.get('task');
-      setTasks(tasksFromApi);
+      try {
+        const fetched = await api.get('/task');
+
+        if (fetched.status === 200) dispatch(replaceAll(fetched.data));
+      } catch (error) {
+        console.error(error);
+      }
     };
     fetchData();
-  }, []);
+  }, [reloadList]);
+
   return (
     <div>
+      {addTask && <TaskForm /> }
       {tasks.map((task, index) => (
         <Task task={{ ...task, index }} />
       ))}
