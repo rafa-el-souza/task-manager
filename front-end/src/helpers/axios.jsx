cff38b11adcd4c3345ac4d71f44f5b521777adcc
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-05-24 17:29:56 -0300

    fix: axios instance base url

--- front-end/src/helpers/axios.jsx
+++ front-end/src/helpers/axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `http://localhost:${process.env.PORT || '3001'}/task`,
+  baseURL: `http://localhost:${process.env.PORT || '3001'}`,
   timeout: 1000,
 });
 
