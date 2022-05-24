6c50c7ee758576f9e47c787d436c18b5f6d4d597
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-05-24 17:44:26 -0300

    feat: add UpdateTaskButton and DeleteTaskButton to TaskOptions passing props

--- front-end/src/components/TaskOptions.jsx
+++ front-end/src/components/TaskOptions.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function TaskOptions() {
+import DeleteTaskButton from './DeleteTaskButton';
+import UpdateTaskButton from './UpdateTaskButton';
+
+function TaskOptions({ _id, setIsUpdating }) {
   return (
-    <div>TaskOptions</div>
+    <>
+      <UpdateTaskButton setIsUpdating={setIsUpdating} />
+      <DeleteTaskButton _id={_id} />
+    </>
   );
 }
+
+TaskOptions.propTypes = {
+  _id: PropTypes.string.isRequired,
+  setIsUpdating: PropTypes.func.isRequired,
+};
 
 export default TaskOptions;
