b1053e9d16355cc721c488de97accec0b8bed211
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-05-23 22:38:47 -0300

    feat: receive task through props | render task's properties | add proptypes

--- front-end/src/components/Task.jsx
+++ front-end/src/components/Task.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Task() {
+function Task({ task }) {
   return (
-    <div>Task</div>
+    <>
+      <div>{task.index}</div>
+      <div>{task._id}</div>
+      <div>{task.name}</div>
+      <div>{task.description}</div>
+      <div>{task.createdAt}</div>
+      <div>{task.updatedAt}</div>
+      <div>{task.status}</div>
+    </>
   );
 }
+
+Task.propTypes = {
+  task: PropTypes.shape({
+    _id: PropTypes.string,
+    name: PropTypes.string,
+    description: PropTypes.string,
+    createdAt: PropTypes.string,
+    updatedAt: PropTypes.string,
+    status: PropTypes.string,
+    index: PropTypes.number,
+  }).isRequired,
+};
 
 export default Task;
