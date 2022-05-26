852e37de2b541af50afca62add1894fbd805f8d4
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-05-26 07:01:40 -0300

    feat: add formatter helper for sorting and and formatting functions

---
+++ front-end/src/helpers/format.jsx
@@ -0,0 +1,67 @@
+export const unformattedStatuses = ['pendente', 'andamento', 'pronto'];
+
+export const formattedStatuses = ['🔴', '🟡', '✔️'];
+
+export const formattedMonths = ['Jan', 'Feb', 'Mar', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+export const formatDate = (date) => `${formattedMonths[date.getMonth()]} ${date.getDate()}, at ${date.getHours()}h${date.getMinutes()}`;
+
+export const formatStatus = (_status) => formattedStatuses[unformattedStatuses.indexOf(_status)];
+
+export const formatTaskForSorting = ({
+  createdAt, updatedAt, ...rest
+}) => ({
+  createdAt: new Date(createdAt),
+  updatedAt: new Date(updatedAt),
+  ...rest,
+});
+
+export const formatTaskForDisplay = ({
+  createdAt, updatedAt, ...rest
+}) => ({
+  createdAt: formatDate(createdAt),
+  updatedAt: formatDate(updatedAt),
+  ...rest,
+});
+
+export const sortTasks = (tasks, sortBy, ascendingOrder) => {
+  if (unformattedStatuses.includes(sortBy)) {
+    return tasks.sort((a, b) => {
+      if (a.status === sortBy && b.status !== sortBy) {
+        return -1;
+      }
+
+      if (a.status !== sortBy && b.status === sortBy) {
+        return 1;
+      }
+
+      return 0;
+    });
+  }
+
+  if (ascendingOrder) {
+    return tasks.sort((a, b) => {
+      if (a[sortBy] < b[sortBy]) {
+        return -1;
+      }
+
+      if (a[sortBy] > b[sortBy]) {
+        return 1;
+      }
+
+      return 0;
+    });
+  }
+
+  return tasks.sort((a, b) => {
+    if (a[sortBy] < b[sortBy]) {
+      return 1;
+    }
+
+    if (a[sortBy] > b[sortBy]) {
+      return -1;
+    }
+
+    return 0;
+  });
+};
