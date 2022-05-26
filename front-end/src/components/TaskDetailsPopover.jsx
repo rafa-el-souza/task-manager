61025839c6cd9e5c9d77481b9e62b49674d4e73c
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-05-26 06:51:06 -0300

    refactor: add TaskDetailsPopover to show tasks details on hovering over task name

--- front-end/src/components/TaskDetailsPopover.jsx
+++ front-end/src/components/TaskDetailsPopover.jsx
@@ -1,28 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function TaskDetailsModal({ task }) {
+function TaskDetailsPopover({ task }) {
   const {
-    _id, description, createdAt, updatedAt,
+    description, createdAt,
   } = task;
 
   return (
     <>
-      <div>{_id}</div>
       <div>{description}</div>
       <div>{createdAt}</div>
-      <div>{updatedAt}</div>
     </>
   );
 }
 
-TaskDetailsModal.propTypes = {
+TaskDetailsPopover.propTypes = {
   task: PropTypes.shape({
-    _id: PropTypes.string,
     description: PropTypes.string,
     createdAt: PropTypes.string,
-    updatedAt: PropTypes.string,
   }).isRequired,
 };
 
-export default TaskDetailsModal;
+export default TaskDetailsPopover;
