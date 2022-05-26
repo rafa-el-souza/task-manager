c85fbdf9588970e62a8044c2991371152bed8071
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-05-26 07:05:31 -0300

    feat: add sortBy and ascendingOrder global states to keep track of sorting parameters for tasks array | rename replaceAll action and add formatting and sorting to it before saving to store | add and export actions to set sortBy and ascendingOrder

--- front-end/src/redux/reducers/taskReducer.jsx
+++ front-end/src/redux/reducers/taskReducer.jsx
@@ -1,31 +1,51 @@
 /* eslint-disable no-param-reassign */
 import { createSlice } from '@reduxjs/toolkit';
+
+import { formatTaskForDisplay, formatTaskForSorting, sortTasks } from '../../helpers/format';
 import { readLocalStorage } from '../../helpers/localStorage';
 
 const initialState = {
   tasks: readLocalStorage('tasks') || [],
   addTask: false,
   reloadList: false,
+  sortBy: 'createdAt',
+  ascendingOrder: false,
 };
 
 export const tasksSlice = createSlice({
   name: 'tasks',
   initialState,
   reducers: {
-    replaceAll: (state, action) => {
-      state.tasks = action.payload;
+    getTasks: (state, action) => {
+      const formattedForSorting = action.payload.map(formatTaskForSorting);
+
+      const sorted = sortTasks(formattedForSorting, state.sortBy, state.ascendingOrder);
+
+      const formattedForDisplay = sorted.map(formatTaskForDisplay);
+
+      state.tasks = formattedForDisplay;
     },
+
     toggleAddTask: (state) => {
       state.addTask = !state.addTask;
     },
+
     reloadList: (state) => {
       state.reloadList = !state.reloadList;
+    },
+
+    setSortBy: (state, action) => {
+      state.sortBy = action.payload;
+    },
+
+    setAscendingOrder: (state, action) => {
+      state.ascendingOrder = action.payload;
     },
   },
 });
 
 export const {
-  replaceAll, toggleAddTask, reloadList,
+  getTasks, toggleAddTask, reloadList, setSortBy, setAscendingOrder,
 } = tasksSlice.actions;
 
 export default tasksSlice.reducer;
