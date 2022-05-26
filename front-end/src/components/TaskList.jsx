25e0e1b5a4af8f7257ee116d4f1e7ae4de2d860a
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-05-26 06:48:44 -0300

    refactor: useSelector to get the whole tasks store instead of one of its keys at a time | add url and http status as variables to api helper | fetch data when sortBy and ascendingOrder global states change | add key prop to tasks map

--- front-end/src/components/TaskList.jsx
+++ front-end/src/components/TaskList.jsx
@@ -1,38 +1,41 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import api from '../helpers/axios';
+import api, { http, URL } from '../helpers/api';
 import Task from './Task';
-import { replaceAll } from '../redux/reducers/taskReducer';
+import { getTasks } from '../redux/reducers/taskReducer';
 import TaskForm from './TaskForm';
 
 function TaskList() {
   const dispatch = useDispatch();
 
-  const tasks = useSelector((state) => state.tasks.tasks);
+  const tasksStore = useSelector((state) => state.tasks);
 
-  const addTask = useSelector((state) => state.tasks.addTask);
-
-  const reloadList = useSelector((state) => state.tasks.reloadList);
+  const {
+    tasks, addTask, sortBy, ascendingOrder, reloadList,
+  } = tasksStore;
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const fetched = await api.get('/task');
+        const fetched = await api.get(URL);
 
-        if (fetched.status === 200) dispatch(replaceAll(fetched.data));
+        if (fetched.status === http.OK) dispatch(getTasks(fetched.data));
       } catch (error) {
         console.error(error);
       }
     };
     fetchData();
-  }, [reloadList]);
+  }, [reloadList, sortBy, ascendingOrder]);
 
   return (
     <div>
       {addTask && <TaskForm /> }
-      {tasks.map((task, index) => (
-        <Task task={{ ...task, index }} />
+      {tasks.map((task) => (
+        <Task
+          task={{ ...task }}
+          key={task._id}
+        />
       ))}
     </div>
   );
