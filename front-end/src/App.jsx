878b4d8495bfd6053c3fb2a87618dbc8ec2acb9a
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-05-26 07:07:26 -0300

    feat: add TaskSorter to App component

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+
 import AddTaskButton from './components/AddTaskButton';
 import TaskList from './components/TaskList';
+import TaskSorter from './components/TaskSorter';
 
 function App() {
   return (
     <div className="App">
       <AddTaskButton />
+      <TaskSorter />
       <TaskList />
     </div>
   );
