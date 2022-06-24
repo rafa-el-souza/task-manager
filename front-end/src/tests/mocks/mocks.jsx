9117a76beef6bfc52c4a1578086d3dd21cf4bcb2
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-06-24 16:02:22 -0300

    feat: add mocked values for api responses

---
+++ front-end/src/tests/mocks/mocks.jsx
@@ -0,0 +1,12 @@
+export const mockTaskOne = {
+  _id: '1', name: 'Task 001', description: 'Description 001', status: 'pendente',
+};
+
+export const mockTaskTwo = {
+  _id: '2', name: 'Task 002', description: 'Description 002', status: 'pendente',
+};
+
+export const httpStatus = {
+  OK: 200,
+  CREATED: 201,
+};
