6fd1d57c34b3b21ff6968bc90857f24da801675d
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-06-30 11:00:39 -0300

    feat: add labels to radio inputs, hide sorter when task list is empty, add fade animation to inputs

--- front-end/src/components/TaskSorter.jsx
+++ front-end/src/components/TaskSorter.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { CSSTransition } from 'react-transition-group';
+import { DotsCircleHorizontalIcon } from '@heroicons/react/solid';
+
 import { setAscendingOrder, setSortBy } from '../redux/reducers/taskReducer';
 
 function TaskSorter() {
   const dispatch = useDispatch();
+
+  const { tasks } = useSelector((state) => state.tasks);
 
   const handleSort = (e) => {
     const { name, value } = e.target;
@@ -13,37 +19,60 @@ function TaskSorter() {
     return dispatch(setAscendingOrder(value === 'true'));
   };
 
-  return (
-    <>
-      <div onChange={handleSort}>
-        <input type="radio" value="createdAt" name="sortBy" />
-        {' '}
-        createdAt
-        <input type="radio" value="name" name="sortBy" />
-        {' '}
-        name
-      </div>
-      <div onChange={handleSort}>
-        <input type="radio" value name="ascendingOrder" />
-        {' '}
-        ascending
-        <input type="radio" value={false} name="ascendingOrder" />
-        {' '}
-        descending
-      </div>
-      <div onChange={handleSort}>
-        <input type="radio" value="pendente" name="sortBy" />
-        {' '}
-        pendente
-        <input type="radio" value="andamento" name="sortBy" />
-        {' '}
-        andamento
-        <input type="radio" value="pronto" name="sortBy" />
-        {' '}
-        pronto
-      </div>
+  const [visible, setVisible] = useState(false);
 
-    </>
+  return (
+    <div
+      className={(tasks.length === 0) ? 'hidden' : 'flex h-24 mb-10'}
+    >
+      <button
+        type="button"
+        onClick={() => setVisible(!visible)}
+        className=""
+      >
+        <DotsCircleHorizontalIcon className="w-16 fill-blue-500" />
+      </button>
+      <CSSTransition
+        in={visible}
+        timeout={{
+          enter: 500,
+          exit: 500,
+        }}
+        classNames="fade"
+        unmountOnExit
+      >
+        <div onChange={handleSort} className="text-slate-200 flex justify-center items-start flex-col w-24 text-xs ml-5">
+          <label htmlFor="createdAt" className="font-light">
+            <input type="radio" value="createdAt" name="sortBy" id="createdAt" className="mr-2" />
+            createdAt
+          </label>
+          <label className="font-light" htmlFor="name">
+            <input type="radio" value="name" name="sortBy" className="mr-2" id="name" />
+            name
+          </label>
+          <label className="font-light" htmlFor="ascending">
+            <input type="radio" value name="ascendingOrder" className="mr-2" id="ascending" />
+            ascending
+          </label>
+          <label className="font-light" htmlFor="descending">
+            <input type="radio" value={false} name="ascendingOrder" className="mr-2" id="descending" />
+            descending
+          </label>
+          <label className="font-light" htmlFor="pendente">
+            <input type="radio" value="pendente" name="sortBy" className="mr-2" id="pendente" />
+            pendente
+          </label>
+          <label className="font-light" htmlFor="andamento">
+            <input type="radio" value="andamento" name="sortBy" className="mr-2" id="andamento" />
+            andamento
+          </label>
+          <label className="font-light" htmlFor="pronto">
+            <input type="radio" value="pronto" name="sortBy" className="mr-2" id="pronto" />
+            pronto
+          </label>
+        </div>
+      </CSSTransition>
+    </div>
   );
 }
 
