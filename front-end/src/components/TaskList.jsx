997f95a2fb45887ecb205924ed728ae62766e2c4
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-06-30 10:57:30 -0300

    feat: add fade animation on enter and exit of list elements

--- front-end/src/components/TaskList.jsx
+++ front-end/src/components/TaskList.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 import api, { http, URL } from '../helpers/api';
-import Task from './Task';
 import { getTasks } from '../redux/reducers/taskReducer';
+
+import Task from './Task';
 import TaskForm from './TaskForm';
 
 function TaskList() {
@@ -29,14 +31,44 @@ function TaskList() {
   }, [reloadList, sortBy, ascendingOrder]);
 
   return (
-    <div>
-      {addTask && <TaskForm /> }
-      {tasks.map((task) => (
-        <Task
-          task={{ ...task }}
-          key={task._id}
-        />
-      ))}
+    <div
+      className="mb-20 flex flex-col w-full"
+    >
+      <CSSTransition
+        in={addTask}
+        timeout={{
+          enter: 500,
+          exit: 500,
+        }}
+        classNames="fade"
+        unmountOnExit
+      >
+        <div
+          className="flex justify-center items-center w-full"
+        >
+          <TaskForm />
+        </div>
+      </CSSTransition>
+      <TransitionGroup>
+        {tasks.map((task) => (
+          <CSSTransition
+            key={task._id}
+            in
+            timeout={{
+              enter: 500,
+              exit: 500,
+            }}
+            classNames="fade"
+            unmountOnExit
+          >
+            <Task
+              task={{ ...task }}
+              key={task._id}
+            />
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
+
     </div>
   );
 }
