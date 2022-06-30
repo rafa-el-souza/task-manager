23253968becf6ebac13258af14806350185a21da
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-06-30 11:02:20 -0300

    refactor: import animations.css, add fade animation on appear

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
+/* eslint-disable max-len */
+import React, { useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
 
 import AddTaskButton from './components/AddTaskButton';
 import TaskList from './components/TaskList';
 import TaskSorter from './components/TaskSorter';
 
+import './styles/animations.css';
+
 function App() {
+  const [visible] = useState(true);
+
   return (
-    <div className="App">
-      <AddTaskButton />
-      <TaskSorter />
-      <TaskList />
+    <div
+      className="bg-slate-900 h-screen flex flex-col justify-center items-center w-full"
+    >
+      <CSSTransition
+        in={visible}
+        appear
+        timeout={{
+          appear: 1000,
+        }}
+        classNames="fade"
+      >
+        <div className="flex flex-col justify-center items-center h-max absolute top-16 bg-slate-900 w-full">
+          <TaskSorter />
+          <AddTaskButton />
+          <TaskList />
+        </div>
+      </CSSTransition>
     </div>
   );
 }
